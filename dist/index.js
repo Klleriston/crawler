9697708781269259b46875e222d614a8eef1cdbe
Klleriston <Klleandrade@outlook.com>
2023-12-15 18:22:25 -0300

    chamada na api de clima

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,37 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
 const app = (0, express_1.default)();
 const port = 3000;
+const apiKey = 'f1cdb1003a7e05c670b29a5aabad3ce1';
+let lon = 10.99;
+let lat = 44.34;
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
 });
 app.get('/home', (req, res) => {
     res.status(200).send('Olá, estou usando express com o type');
 });
+app.get('/teste', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const apiResponse = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+        res.json(apiResponse.data);
+    }
+    catch (error) {
+        console.error('Erro ao chamar a API:', error);
+        res.status(500).json({ error: 'Erro ao chamar a API' });
+    }
+}));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
+const apiKey = 'f1cdb1003a7e05c670b29a5aabad3ce1';
+let lon = 10.99;
+let lat = 44.34;
+
 
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
@@ -9,4 +14,14 @@ app.listen(port, () => {
 
 app.get('/home', (req, res) => {
     res.status(200).send('Olá, estou usando express com o type');
+});
+app.get('/teste', async (req, res) => {
+    try {
+        const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    
+        res.json(apiResponse.data);
+      } catch (error) {
+        console.error('Erro ao chamar a API:', error);
+        res.status(500).json({ error: 'Erro ao chamar a API' });
+      }
 });
