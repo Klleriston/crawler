c4b52074ed925e845906877daf0065e3d0dda0e9
Klleriston <Klleandrade@outlook.com>
2023-12-15 19:16:10 -0300

    commit trazendo json e dados da api

--- dist/index.js
+++ dist/index.js
@@ -19,16 +19,24 @@ const port = 3000;
 const apiKey = 'f1cdb1003a7e05c670b29a5aabad3ce1';
 let lon = 10.99;
 let lat = 44.34;
+let cidade;
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
 });
 app.get('/home', (req, res) => {
     res.status(200).send('Olá, estou usando express com o type');
 });
-app.get('/teste', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get(`/teste`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const apiResponse = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
-        res.json(apiResponse.data);
+        const weatherData = apiResponse.data;
+        const tempCelsius = weatherData.main.tempo - 273.15;
+        const tempFormat = parseFloat(tempCelsius.toFixed(2));
+        const respMod = {
+            cidade: weatherData.name,
+            temperatura: tempFormat,
+        };
+        res.json(respMod);
     }
     catch (error) {
         console.error('Erro ao chamar a API:', error);
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const apiKey = 'f1cdb1003a7e05c670b29a5aabad3ce1';
 let lon = 10.99;
 let lat = 44.34;
 
+let cidade
 
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
@@ -15,13 +16,21 @@ app.listen(port, () => {
 app.get('/home', (req, res) => {
     res.status(200).send('Olá, estou usando express com o type');
 });
-app.get('/teste', async (req, res) => {
+app.get(`/teste`, async (req, res) => {
     try {
         const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
-    
-        res.json(apiResponse.data);
-      } catch (error) {
+
+        const weatherData = apiResponse.data;
+        const tempCelsius = weatherData.main.tempo - 273.15;
+        const tempFormat = parseFloat(tempCelsius.toFixed(2))
+        const respMod = {
+            cidade: weatherData.name,
+            temperatura: tempFormat, //temperatura Null, arrumar isso
+        }
+
+        res.json(respMod);
+    } catch (error) {
         console.error('Erro ao chamar a API:', error);
         res.status(500).json({ error: 'Erro ao chamar a API' });
-      }
+    }
 });
