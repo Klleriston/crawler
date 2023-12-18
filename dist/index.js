b005826377356eac314ceab7c7060c7c9063b33b
Klleriston <Klleandrade@outlook.com>
2023-12-18 17:01:26 -0300

    CIDADE BERLIN LIGADA AO DATABASE

--- dist/index.js
+++ dist/index.js
@@ -14,12 +14,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
+const mongodb_1 = require("mongodb");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
-const apiKey = 'f1cdb1003a7e05c670b29a5aabad3ce1';
-let lon = 10.99;
-let lat = 44.34;
-let cidade; // zooca lon 10.99 lat 44.34
+const port = 3000 || process.env.PORT;
+const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+const mongodbURL = process.env.DB_CONN_STRING;
+let defaultlon = 13.404954;
+let defaultlan = 52.520008;
+const client = new mongodb_1.MongoClient(mongodbURL);
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
 });
@@ -27,19 +31,28 @@ app.get('/home', (req, res) => {
     res.status(200).send('Olá, estou usando express com o type');
 });
 app.get(`/teste`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { lon = defaultlon, lat = defaultlan, cidade } = req.query;
     try {
         const apiResponse = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
         const weatherData = apiResponse.data;
         const tempCelsius = weatherData.main.temp - 273.15;
         const tempFormat = parseFloat(tempCelsius.toFixed(1));
         const respMod = {
-            cidade: weatherData.name,
+            cidade: cidade || weatherData.name,
             temperatura: tempFormat,
         };
+        yield client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection(process.env.CITYS_COLLECTION_NAME);
+        const result = yield collection.insertOne(respMod);
+        console.log(`Inserindo cidade no banco de dados. ID: ${result.insertedId}`);
         res.json(respMod);
     }
     catch (error) {
         console.error('Erro ao chamar a API:', error);
         res.status(500).json({ error: 'Erro ao chamar a API' });
+    }
+    finally {
+        yield client.close();
     }
 }));
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import express from 'express';
+import express, { response } from 'express';
 import axios from 'axios';
+import { MongoClient } from 'mongodb';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
-const apiKey = 'f1cdb1003a7e05c670b29a5aabad3ce1';
-let lon = 10.99;
-let lat = 44.34;
+const port = 3000 || process.env.PORT;
+const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+const mongodbURL = process.env.DB_CONN_STRING;
 
-let cidade // zooca lon 10.99 lat 44.34
+let defaultlon = 13.404954;
+let defaultlan = 52.520008;
+
+const client = new MongoClient(mongodbURL!)
 
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
@@ -17,6 +23,7 @@ app.get('/home', (req, res) => {
     res.status(200).send('Olá, estou usando express com o type');
 });
 app.get(`/teste`, async (req, res) => {
+    const { lon = defaultlon, lat = defaultlan, cidade} = req.query;
     try {
         const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
 
@@ -24,13 +31,22 @@ app.get(`/teste`, async (req, res) => {
         const tempCelsius = weatherData.main.temp - 273.15;
         const tempFormat = parseFloat(tempCelsius.toFixed(1))
         const respMod = {
-            cidade: weatherData.name,
+            cidade: cidade || weatherData.name,
             temperatura: tempFormat, 
         }
+
+        await client.connect();
+        const db= client.db(process.env.DB_NAME);
+        const collection = db.collection(process.env.CITYS_COLLECTION_NAME!);
+
+        const result = await collection.insertOne(respMod);
+        console.log(`Inserindo cidade no banco de dados. ID: ${result.insertedId}`)
 
         res.json(respMod);
     } catch (error) {
         console.error('Erro ao chamar a API:', error);
         res.status(500).json({ error: 'Erro ao chamar a API' });
+    } finally {
+        await client.close();
     }
 });
