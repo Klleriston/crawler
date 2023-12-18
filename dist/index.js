65848f5d2cdbffc30527cf87f0cf9a74ba543faf
Klleriston <Klleandrade@outlook.com>
2023-12-18 20:14:50 -0300

    levando para aws

--- dist/index.js
+++ dist/index.js
@@ -23,19 +23,15 @@ const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 const mongodbURL = process.env.DB_CONN_STRING;
 let defaultlon = 13.404954;
 let defaultlan = 52.520008;
-let city = "Guarulhos";
 const client = new mongodb_1.MongoClient(mongodbURL);
+app.use(express_1.default.json());
 app.listen(port, () => {
     console.log(`O servidor está rodando em http://localhost:${port}`);
 });
-app.get('/home', (req, res) => {
-    res.status(200).send('Olá, estou usando express com o type');
-});
-app.get(`/teste`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get(`/`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { lon = defaultlon, lat = defaultlan, cidade } = req.query;
     try {
-        // const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
-        const apiResponse = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        const apiResponse = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
         const weatherData = apiResponse.data;
         const tempCelsius = weatherData.main.temp - 273.15;
         const tempCelsiusMAX = weatherData.main.temp_max - 273.15;
@@ -45,6 +41,65 @@ app.get(`/teste`, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const temp_MIN_Format = parseFloat(tempCelsiusMIN.toFixed(1));
         const respMod = {
             cidade: cidade || weatherData.name,
+            temperatura: tempFormat,
+            temperatura_max: temp_MAX_Format,
+            temperatura_min: temp_MIN_Format,
+        };
+        yield client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection(process.env.CITYS_COLLECTION_NAME);
+        const result = yield collection.insertOne(respMod);
+        console.log(`Inserindo cidade no banco de dados. ID: ${result.insertedId}`);
+        res.json(respMod);
+    }
+    catch (error) {
+        console.error('Erro ao chamar a API:', error);
+        res.status(500).json({ error: 'Erro ao chamar a API' });
+    }
+    finally {
+        yield client.close();
+    }
+}));
+app.get('/data', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { dataMin, dataMax } = req.query;
+    try {
+        yield client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection(process.env.CITYS_COLLECTION_NAME);
+        function formatToISO(dateString) {
+            return new Date(dateString).toISOString();
+        }
+        const dateFilter = dataMin && dataMax ? {
+            timestamp: {
+                $gte: formatToISO(dataMin.toString()),
+                $lte: formatToISO(dataMax.toString())
+            }
+        } : {};
+        const dataFromDB = yield collection.find(dateFilter).toArray();
+        console.log('Dados do banco de dados com filtro de data:', dataFromDB);
+        res.json(dataFromDB);
+    }
+    catch (error) {
+        console.error('Erro ao consultar o banco de dados:', error);
+        res.status(500).json({ error: 'Erro ao consultar o banco de dados' });
+    }
+    finally {
+        yield client.close();
+    }
+}));
+app.post('/insert', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { lon, lat } = req.body;
+    try {
+        const apiResponse = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+        const weatherData = apiResponse.data;
+        const tempCelsius = weatherData.main.temp - 273.15;
+        const tempCelsiusMAX = weatherData.main.temp_max - 273.15;
+        const tempCelsiusMIN = weatherData.main.temp_min - 273.15;
+        const tempFormat = parseFloat(tempCelsius.toFixed(1));
+        const temp_MAX_Format = parseFloat(tempCelsiusMAX.toFixed(1));
+        const temp_MIN_Format = parseFloat(tempCelsiusMIN.toFixed(1));
+        const respMod = {
+            cidade: weatherData.name,
             temperatura: tempFormat,
             temperatura_max: temp_MAX_Format,
             temperatura_min: temp_MIN_Format,
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import awsServerlessExpress from 'aws-serverless-express';
     import express, { response } from 'express';
     import axios from 'axios';
     import { MongoClient } from 'mongodb';
     import dotenv from "dotenv";
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 
     dotenv.config();
 
@@ -12,20 +14,18 @@
 
     let defaultlon = 13.404954;
     let defaultlan = 52.520008;
-    let city = "Guarulhos"
 
     const client = new MongoClient(mongodbURL!)
+    app.use(express.json());
 
-    app.listen(port, () => {
-        console.log(`O servidor está rodando em http://localhost:${port}`);
-    })
+    // app.listen(port, () => {
+    //     console.log(`O servidor está rodando em http://localhost:${port}`);
+    // })
 
     app.get(`/`, async (req, res) => {
         const { lon = defaultlon, lat = defaultlan, cidade} = req.query;
         try {
-        // const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
-            const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
-
+         const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
             const weatherData = apiResponse.data;
 
             const tempCelsius = weatherData.main.temp - 273.15;
@@ -69,7 +69,6 @@
             const collection = db.collection(process.env.CITYS_COLLECTION_NAME!);
 
             function formatToISO(dateString: string) {
-                // Converte a string para uma data e depois para uma string ISO 8601
                 return new Date(dateString).toISOString();
             }
 
@@ -91,4 +90,49 @@
             await client.close();
         }
     });
+
+    app.post('/insert', async (req, res) => {
+        const { lon, lat } = req.body;
+    
+        try {
+            const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+            const weatherData = apiResponse.data;
+    
+            const tempCelsius = weatherData.main.temp - 273.15;
+            const tempCelsiusMAX = weatherData.main.temp_max - 273.15;
+            const tempCelsiusMIN = weatherData.main.temp_min - 273.15;
+    
+            const tempFormat = parseFloat(tempCelsius.toFixed(1));
+            const temp_MAX_Format = parseFloat(tempCelsiusMAX.toFixed(1));
+            const temp_MIN_Format = parseFloat(tempCelsiusMIN.toFixed(1));
+    
+            const respMod = {
+                cidade: weatherData.name,
+                temperatura: tempFormat,
+                temperatura_max: temp_MAX_Format,
+                temperatura_min: temp_MIN_Format,
+            };
+    
+            await client.connect();
+            const db = client.db(process.env.DB_NAME);
+            const collection = db.collection(process.env.CITYS_COLLECTION_NAME!);
+    
+            const result = await collection.insertOne(respMod);
+            console.log(`Inserindo cidade no banco de dados. ID: ${result.insertedId}`);
+    
+            res.json(respMod);
+        } catch (error) {
+            console.error('Erro ao chamar a API:', error);
+            res.status(500).json({ error: 'Erro ao chamar a API' });
+        } finally {
+            await client.close();
+        }
+    });
+    const server = awsServerlessExpress.createServer(app);
+
+// Exporte a função handler para a AWS Lambda
+exports.handler = (event: APIGatewayProxyEvent, context: Context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
+    
 
