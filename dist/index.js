62fd5738495cc09083952d7d1751cc38d59ea657
Klleriston <Klleandrade@outlook.com>
2023-12-19 18:42:26 -0300

    agora sim

--- dist/index.js
+++ dist/index.js
@@ -12,11 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const aws_serverless_express_1 = __importDefault(require("aws-serverless-express"));
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
 const mongodb_1 = require("mongodb");
 const dotenv_1 = __importDefault(require("dotenv"));
+const node_cron_1 = __importDefault(require("node-cron"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = 3000 || process.env.PORT;
@@ -121,7 +121,42 @@ app.post('/insert', (req, res) => __awaiter(void 0, void 0, void 0, function* ()
         yield client.close();
     }
 }));
-const server = aws_serverless_express_1.default.createServer(app);
-exports.handler = (event, context) => {
-    aws_serverless_express_1.default.proxy(server, event, context);
-};
+function fetchDataAndSave(lon, lat, cidade) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const apiResponse = yield axios_1.default.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+            const weatherData = apiResponse.data;
+            const tempCelsius = weatherData.main.temp - 273.15;
+            const tempCelsiusMAX = weatherData.main.temp_max - 273.15;
+            const tempCelsiusMIN = weatherData.main.temp_min - 273.15;
+            const tempFormat = parseFloat(tempCelsius.toFixed(1));
+            const temp_MAX_Format = parseFloat(tempCelsiusMAX.toFixed(1));
+            const temp_MIN_Format = parseFloat(tempCelsiusMIN.toFixed(1));
+            const respMod = {
+                cidade: cidade || weatherData.name,
+                temperatura: tempFormat,
+                temperatura_max: temp_MAX_Format,
+                temperatura_min: temp_MIN_Format,
+            };
+            yield client.connect();
+            const db = client.db(process.env.DB_NAME);
+            const collection = db.collection(process.env.CITYS_COLLECTION_NAME);
+            const result = yield collection.insertOne(respMod);
+            console.log(`Inserindo cidade no banco de dados. ID: ${result.insertedId}`);
+        }
+        catch (error) {
+            console.error('Erro ao chamar a API:', error);
+        }
+        finally {
+            yield client.close();
+        }
+    });
+}
+;
+node_cron_1.default.schedule('0 * * * *', () => __awaiter(void 0, void 0, void 0, function* () {
+    const lon = -23.5505;
+    const lat = -46.6333;
+    const cidade = 'Sao Paulo';
+    yield fetchDataAndSave(lon, lat, cidade);
+    console.log('Tarefa agendada executada com sucesso!');
+}));
